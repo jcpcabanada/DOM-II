18be76d8483b5cbf8712de1f9cde2f33b262716b
jcpcabanada <jcpcabanada@gmail.com>
2021-10-26 16:20:38 -0700

    push push

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+const links = document.querySelectorAll('a');
+
+Array.from(links).forEach(link => {
+    link.addEventListener('mouseover', (event) => {
+        event.target.style.color = "red";
+    })
+    // event.preventDefault();
+})
